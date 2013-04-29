4f5774b8998a29986da0dd330b7b2a368d44b232
Matt Belisle <mgbelisle@gmail.com>
2013-04-29 14:46:32 -0600

    Need to add Fly to CastLog

--- castlog.go
+++ castlog.go
@@ -11,6 +11,14 @@ type CastLog struct {
 }
 
 func (l Lake) ShowCastLogs(castLogs []CastLog) {
-	t := template.Must(template.ParseFiles("castlogs.html"))
+	t := template.Must(template.New("svg").Parse(svgTemplate))
 	t.Execute(os.Stdout, CastLog{})
 }
+
+const svgTemplate =
+`<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <rect width="500" height="300" fill="blue" fill-opacity="0.5" />
+  <circle cx="250" cy="150" r="2" fill="red" />
+  <circle cx="250" cy="155" r="2" fill="green" />
+</svg>
+`
