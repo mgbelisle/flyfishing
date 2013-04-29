50cda89fcaa445297461a1e5aa7e68dcbe504980
Matt Belisle <mgbelisle@gmail.com>
2013-04-29 12:39:44 -0600

    making async cast

--- flyfishing/main.go
+++ flyfishing/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	lake := flyfishing.NewLake()
-	casts := castNTimes(100, lake)
+	casts := castNTimesAsync(100, lake)
 	log.Println(len(casts))
 }
 
@@ -27,11 +27,17 @@ func castNTimes(n int, lake flyfishing.Lake) []cast {
 }
 
 func castNTimesAsync(n int, lake flyfishing.Lake) []cast {
+	castChan := make(chan cast)
+	for i := 0; i < n; i++ {
+		go func() {
+			loc := lake.RandLoc()
+			fish := lake.CastInto(flyfishing.Caddis{}, lake.RandLoc())
+			castChan <- cast{loc, fish}
+		}()
+	}
 	casts := []cast{}
 	for i := 0; i < n; i++ {
-		loc := lake.RandLoc()
-		fish := lake.CastInto(flyfishing.Caddis{}, lake.RandLoc())
-		casts = append(casts, cast{loc, fish})
+		casts = append(casts, <-castChan)
 	}
 	return casts
 }
